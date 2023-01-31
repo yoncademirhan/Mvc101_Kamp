100e5dd500aa5bd84c7effe65c80661c2eb662d4
yoncademirhan <yonca.demirhan61@gmail.com>
2023-01-31 13:21:35 +0300

    Initial commit

---
+++ MvcProjeKampi/DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>   //parametreler T den alınır.
+    {
+        List<T> List();
+
+        void Insert(T p);
+
+        void Delete(T p);
+
+
+        void Update(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);     //İstenilen şartlarda listeleme yapar.
+
+    }
+}
---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();          //context sınıfından c isimli nesne türetildi.
+        DbSet<Category> _object;           //DbSet türünde Category sınıfından nesne türerildi
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);     //parametreden gelen degeri _object içerisinde bulunan sınıfa ekle (Category) sınıfı
+            c.SaveChanges();    //Değişiklileri Kaydet
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();        //ToList verileri listelemek için kullanılan metottur.
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
